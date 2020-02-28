6fe14dbe9c10d3c6e6ccea030887c5e17757bb54
Olivier Jansen <oli4jansen.nl@gmail.com>
2020-02-28 11:43:54 +0100

    check python

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+script_directory=$(dirname -- "$0")
+
 echo "Going to run VIBE on video directory $1"
 
-$PWD/vibe-env/bin/python $PWD/demo.py --videos $1 --output_folder output/
+$script_directory/vibe-env/bin/python $PWD/demo.py --videos $1 --output_folder output/
