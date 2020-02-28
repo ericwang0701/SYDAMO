d06032814e9eea1f695355ee954c66579fc4b3b6
Olivier Jansen <oli4jansen.nl@gmail.com>
2020-02-28 10:08:11 +0100

    modified run script

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-files = ("$1/*")
+echo "Going to run VIBE on video directory $1"
 
-function join_by { local IFS="$1"; shift; echo "$*"; }
-
-videos = join_by "," "${arr[@]}"
-echo "Going to run VIBE on video ${videos}"
-
-python demo.py --videos videos --output_folder output/
+python demo.py --videos $1 --output_folder output/
