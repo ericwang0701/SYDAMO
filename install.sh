362ac949059444476e5287ddbb2ae6a01c0a8e5b
Olivier Jansen <oli4jansen.nl@gmail.com>
2020-02-28 11:51:49 +0100

    compare

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ cd script_directory
 if [ ! -d "./vibe-env" ]
 then
   echo "Creating virtual environment"
-  if [ "$(python3 -V)" =~ "Python 3" ]
+  if [ "$(python3 -V)" == "Python 3*" ]
   then
     python3 -m venv vibe-env
-  elif [ "$(python -V)" =~ "Python 3" ]
+  elif [ "$(python -V)" == "Python 3*" ]
   then
     python -m venv vibe-env
   else
